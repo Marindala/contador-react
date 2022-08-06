5ea1a1eeec58a7752a700e7312193e7ba90b03bf
Marina <marinalopezka@gmail.com>
2022-08-06 17:55:12 -0300

    contador-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './imagenes/logopng.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      <div className='logo-contenedor'>
+        <img className='logo-free'
+        src={logo}
+        alt='logo'
+        />
+
+      </div>
+    
     </div>
   );
 }
 
 export default App;
+
+//cambiar comillas para ser consistentes.
